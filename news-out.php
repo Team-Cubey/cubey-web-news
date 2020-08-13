cda9030c448fcbaea04895ce29a15812a6de8a2f
Hubz <hubsterhub@gmail.com>
2020-08-13 17:14:42 +0100

    Create news-out.php

---
+++ news-out.php
@@ -0,0 +1,6 @@
+{
+  "name": "News System Released!",
+  "creator": "Hubz"
+}
+
+The new news system is just about done! This is the first *actual* post on the news system which isn't just a test!<br>I'll be posting updates to the game and stuff like that here, when I can be bothered, lets just hope the system works!
